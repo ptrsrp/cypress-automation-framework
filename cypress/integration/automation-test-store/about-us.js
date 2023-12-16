33c4530284b7fe0f70e40ed3887ce275886f59d0
unknown <yuusufyan13@gmail.com>
2023-12-16 20:37:04 +0700

    Add About-Us service

---
+++ cypress/integration/automation-test-store/about-us.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+/// <reference types="@cypress/xpath" />
+
+
+
+describe("Test Contact Us form via Automation Test Store", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+      //cypress code
+      cy.visit("https://automationteststore.com/");
+      //cy.get('.info_links_footer > :nth-child(5) > a').click()
+      cy.xpath("//a[contains(@href,'contact')]").click()
+      cy.get('#ContactUsFrm_first_name').type('Joe')
+      cy.get('#ContactUsFrm_email').type('joejoejoe@gmail.cum')
+      cy.get('#ContactUsFrm_enquiry').type('Dicoba-coba dulu yaaa!')
+      //cy.get("button[title='Submit']").click()
+      cy.get("button[type = 'reset']").click()
+  });
+})
